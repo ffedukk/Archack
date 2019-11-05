9c3be1ad3f9b09ac3bdb7c96f1e9b39262b47264
Fedor <ffedukk@fedors-imac.local>
2019-11-05 10:11:03 -0800

    Initial Commit

---
+++ Archack 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Archack 2
+//
+//  Created by Fedor on 05/11/2019.
+//  Copyright © 2019 Fedor Semenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
